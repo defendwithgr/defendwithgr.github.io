e1e3398225f91fd1a493d366e64790f1c4d5de52
defendwithgr <41144259+defendwithgr@users.noreply.github.com>
2026-02-12 03:31:51 +0000

    Deploying to gh-pages from @ defendwithgr/defendwithgr.github.io@387da69abdd1707de5d75c936b0ac1bd2fb427a6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-publications",
-          title: "publications",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/publications/";
-          },
-        },{id: "nav-patents",
+  },{id: "nav-patents",
           title: "patents",
           description: "A list of my patents.",
           section: "Navigation",
